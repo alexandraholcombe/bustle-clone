876b2aecaee8b479dc04367cfcc66ae82f2b20ac
Alexandra Holcombe and Cassie Musolf <alexandra+cassie@>
2017-03-21 15:55:52 -0700

    Create bigFeature section for 3rdindex

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,6 +2,9 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    return this.store.findAll('content');
+    return Ember.RSVP.hash({
+      feature: this.store.query('content', {limitToFirst:3}),
+      bigFeature: this.store.query('content', {startAt: "3", endAt: "3"}),
+    });
   },
 });
